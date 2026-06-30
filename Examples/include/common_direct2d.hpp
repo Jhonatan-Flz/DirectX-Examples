925a77ad3e780ebb6ead59f6d90e75d40c9cb327
Jhonatan <jhonatanflorez4000@gmail.com>
2026-06-30 14:14:17 -0500

    Refactor project structure

---
+++ Examples/include/common_direct2d.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <windows.h>
+#include <d2d1.h>
+#include <dwrite.h>
+#include <cmath>
+
+template <typename T>
+inline void safe_release ( T*& pointer ) {
+
+    if ( pointer ) {
+
+        pointer -> Release (  );
+        pointer = nullptr;
+ 
+	}
+
+}
+
+inline float minimum_float ( float first, float second ) { return ( first < second ) ? first : second; }
