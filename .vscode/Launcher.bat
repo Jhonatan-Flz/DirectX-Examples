4473ea9a20fdb7c51c535287a0f9e57759506fc7
Jhonatan <jhonatanflorez4000@gmail.com>
2026-07-07 17:59:46 -0500

    Minor fix at launcher.bat

--- .vscode/Launcher.bat
+++ .vscode/Launcher.bat
@@ -4,7 +4,7 @@ title %1
 :: Flags
 :: -ldcomp -ldxguid -ldxgi -ld3d11 -lwindowscodecs -luser32 -mwindows -lole32 -static-libgcc -static-libstdc++
 g++ "%1" -o "%2" -municode -ldwrite -ld2d1 -lgdi32 
-if exist "%2" ( move "%2" Output\ > nul & cd Output & .\"%2" )
+if exist "%2" ( move "%2" output\ > nul & cd output & .\"%2" )
 
 echo.
 pause
